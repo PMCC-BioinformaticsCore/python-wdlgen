7dac3d7cd0d20d9c7e311db611f747ea25036ebb
Michael Franklin <michael.franklin@petermac.org>
2020-02-17 10:52:18 +1100

    Format task.py with `black`

--- wdlgen/task.py
+++ wdlgen/task.py
@@ -32,9 +32,9 @@ class Task(WdlBase):
             l = []
             for k, v in self.kwargs.items():
                 val = v
-                if hasattr(v, 'get_string'):
+                if hasattr(v, "get_string"):
                     val = v.get_string()
-                l.append('{k}: {v}'.format(k=k, v=val))
+                l.append("{k}: {v}".format(k=k, v=val))
             return l
 
         def add_docker(self, docker):
@@ -68,8 +68,13 @@ class Task(WdlBase):
         """
 
         class CommandArgument(WdlBase):
-            def __init__(self, prefix: str=None, value: str=None, position: int=None,
-                         separate_value_from_prefix: bool=True):
+            def __init__(
+                self,
+                prefix: str = None,
+                value: str = None,
+                position: int = None,
+                separate_value_from_prefix: bool = True,
+            ):
                 self.prefix: Optional[str] = prefix
                 self.position: Optional[int] = position
                 self.value = value
@@ -82,10 +87,25 @@ class Task(WdlBase):
                 return (pre + sp + val).strip()
 
         class CommandInput(CommandArgument):
-            def __init__(self, name: str, optional: bool=False, prefix: str=None, position: int=None,
-                         separate_value_from_prefix: bool=True, default=None, separator=None, true=None, false=None, separate_arrays=None):
-                super().__init__(prefix=prefix, value=None, position=position,
-                                 separate_value_from_prefix=separate_value_from_prefix)
+            def __init__(
+                self,
+                name: str,
+                optional: bool = False,
+                prefix: str = None,
+                position: int = None,
+                separate_value_from_prefix: bool = True,
+                default=None,
+                separator=None,
+                true=None,
+                false=None,
+                separate_arrays=None,
+            ):
+                super().__init__(
+                    prefix=prefix,
+                    value=None,
+                    position=position,
+                    separate_value_from_prefix=separate_value_from_prefix,
+                )
                 self.name = name
                 self.optional = optional
                 self.default = default
@@ -95,18 +115,28 @@ class Task(WdlBase):
                 self.separate_arrays = separate_arrays
 
             @staticmethod
-            def from_input(inp: Input, prefix: str=None, position: int=None):
-                return Task.Command.CommandInput(inp.name, inp.type.optional, prefix, position)
+            def from_input(inp: Input, prefix: str = None, position: int = None):
+                return Task.Command.CommandInput(
+                    inp.name, inp.type.optional, prefix, position
+                )
 
             def get_string(self):
-                name, array_sep, default, true, false = self.name, self.separator, self.default, self.true, self.false
+                name, array_sep, default, true, false = (
+                    self.name,
+                    self.separator,
+                    self.default,
+                    self.true,
+                    self.false,
+                )
 
                 pr = self.prefix if self.prefix else ""
                 bc = pr + (" " if self.separate and self.prefix else "")
 
                 if self.separate_arrays:
                     if array_sep or default or true or false:
-                        print("separate_array take preferences over: separator, default, true, false")
+                        print(
+                            "separate_array take preferences over: separator, default, true, false"
+                        )
                     if self.optional:
                         # Ugly optional workaround: https://github.com/openwdl/wdl/issues/25#issuecomment-315424063
                         # Additional workaround for 'length(select_first({name}, [])' as length requires a non-optional array
@@ -127,7 +157,7 @@ class Task(WdlBase):
                     if isinstance(default, bool):
 
                         val = "true" if default else "false"
-                    options.append(f'default={val}')
+                    options.append(f"default={val}")
                 if array_sep:
                     options.append(f'sep="{array_sep}"')
                 if true or false:
@@ -137,30 +167,52 @@ class Task(WdlBase):
                 stroptions = "".join(o + " " for o in options)
 
                 if self.optional and not default:
-                    prewithquotes = f'"{bc}" + ' if bc.strip() else ''
-                    return f'~{{{stroptions}{prewithquotes}{name}}}'
+                    prewithquotes = f'"{bc}" + ' if bc.strip() else ""
+                    return f"~{{{stroptions}{prewithquotes}{name}}}"
                 else:
                     return bc + f"~{{{stroptions}{name}}}"
 
-        def __init__(self, command, inputs: Optional[List[CommandInput]]=None, arguments: Optional[List[CommandArgument]]=None):
+        def __init__(
+            self,
+            command,
+            inputs: Optional[List[CommandInput]] = None,
+            arguments: Optional[List[CommandArgument]] = None,
+        ):
             self.command = command
             self.inputs = inputs if inputs else []
             self.arguments = arguments if arguments else []
 
-        def get_string(self, indent: int=0):
+        def get_string(self, indent: int = 0):
             tb = "  "
             base_command = self.command if self.command else ""
             if not (self.inputs or self.arguments):
                 return indent * tb + base_command
 
             # build up command
-            args = sorted([*self.inputs, *self.arguments], key=lambda a: a.position if a.position else 0)
-            command: str = base_command if isinstance(base_command, str) else " ".join(base_command)
+            args = sorted(
+                [*self.inputs, *self.arguments],
+                key=lambda a: a.position if a.position else 0,
+            )
+            command: str = base_command if isinstance(base_command, str) else " ".join(
+                base_command
+            )
             tbed_arg_indent = tb * (indent + 1)
 
-            return indent * tb + command + "".join([" \\\n" + tbed_arg_indent + a.get_string() for a in args])
+            return (
+                indent * tb
+                + command
+                + "".join([" \\\n" + tbed_arg_indent + a.get_string() for a in args])
+            )
 
-    def __init__(self, name: str, inputs: List[Input]=None, outputs: List[Output]=None, command: Command=None, runtime: Runtime=None, version="draft-2"):
+    def __init__(
+        self,
+        name: str,
+        inputs: List[Input] = None,
+        outputs: List[Output] = None,
+        command: Command = None,
+        runtime: Runtime = None,
+        version="draft-2",
+    ):
         self.name = name
         self.inputs = inputs if inputs else []
         self.outputs = outputs if outputs else []
@@ -186,12 +238,15 @@ task {name} {{
         inputs_block, command_block, runtime_block, output_block = "", "", "", ""
 
         if self.inputs:
-            inputs_block = f"{tb}input {{\n" + "\n".join(2*tb + i.get_string() for i in self.inputs) + f"\n{tb}}}"
+            inputs_block = (
+                f"{tb}input {{\n"
+                + "\n".join(2 * tb + i.get_string() for i in self.inputs)
+                + f"\n{tb}}}"
+            )
 
         if self.outputs:
             output_block = "{tb}output {{\n{outs}\n{tb}}}".format(
-                tb=tb,
-                outs="\n".join((2*tb) + o.get_string() for o in self.outputs)
+                tb=tb, outs="\n".join((2 * tb) + o.get_string() for o in self.outputs)
             )
 
         if self.command:
@@ -200,15 +255,11 @@ task {name} {{
                 com = "\n".join(c.get_string(indent=2) for c in self.command)
             else:
                 com = self.command.get_string(indent=2)
-            command_block = "{tb}command <<<\n{args}\n{tb}>>>".format(
-                tb=tb,
-                args=com
-            )
+            command_block = "{tb}command <<<\n{args}\n{tb}>>>".format(tb=tb, args=com)
 
         if self.runtime:
             runtime_block = "{tb}runtime {{\n{args}\n{tb}}}".format(
-                tb=tb,
-                args="\n".join((2 * tb) + a for a in self.runtime.get_string())
+                tb=tb, args="\n".join((2 * tb) + a for a in self.runtime.get_string())
             )
 
         return self.format.format(
@@ -217,5 +268,5 @@ task {name} {{
             command_block=command_block,
             runtime_block=runtime_block,
             output_block=output_block,
-            version=self.version
+            version=self.version,
         )
