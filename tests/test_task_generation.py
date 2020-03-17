5cb6768f6bf2fe439847059c34752df9533ee42c
Michael Franklin <michael.franklin@petermac.org>
2020-03-17 15:46:59 +1100

    Format with black

--- tests/test_task_generation.py
+++ tests/test_task_generation.py
@@ -15,20 +15,25 @@ from wdlgen import (
 
 
 class TestTaskGeneration(unittest.TestCase):
-
     def test_simple_task(self):
         # Task based on: https://github.com/openwdl/wdl/blob/master/versions/draft-2/SPEC.md#introduction
         t = Task("hello", runtime=Task.Runtime(docker="broadinstitute/my_image"))
-        t.inputs.extend([
-            Input(WdlType.parse_type("String"), "pattern"),
-            Input(WdlType.parse_type("File"), "in")
-        ])
-
-        t.outputs.append(
-            Output(WdlType.parse_type("Array[String]"), "matches", "read_lines(stdout())")
+        t.inputs.extend(
+            [
+                Input(WdlType.parse_type("String"), "pattern"),
+                Input(WdlType.parse_type("File"), "in"),
+            ]
         )
 
-        t.command = Task.Command("egrep", inputs=[Task.Command.CommandInput.from_input(i) for i in t.inputs])
+        t.outputs.append(
+            Output(
+                WdlType.parse_type("Array[String]"), "matches", "read_lines(stdout())"
+            )
+        )
+
+        t.command = Task.Command(
+            "egrep", inputs=[Task.Command.CommandInput.from_input(i) for i in t.inputs]
+        )
 
         print(t.get_string())
         return t
@@ -40,8 +45,12 @@ class TestTaskGeneration(unittest.TestCase):
         w.inputs.append(Input(WdlType.parse_type("String"), "inputGreeting"))
 
         inputs_map = {"taskGreeting": "inputGreeting"}
-        w.calls.append(WorkflowCall("Q.namspaced_task_identifier", "task_alias", inputs_map))
-        w.outputs.append(Output(WdlType.parse_type("File"), "standardOut", "task_alias.standardOut"))
+        w.calls.append(
+            WorkflowCall("Q.namspaced_task_identifier", "task_alias", inputs_map)
+        )
+        w.outputs.append(
+            Output(WdlType.parse_type("File"), "standardOut", "task_alias.standardOut")
+        )
 
         print(w.get_string())
 
@@ -53,35 +62,67 @@ class TestTaskGeneration(unittest.TestCase):
 
         command = Task.Command("echo")
         command.inputs.append(
-            Task.Command.CommandInput("taskGreeting", optional=False, position=None, prefix="-a",
-                                             separate_value_from_prefix=True, default=None))
+            Task.Command.CommandInput(
+                "taskGreeting",
+                optional=False,
+                position=None,
+                prefix="-a",
+                separate_value_from_prefix=True,
+                default=None,
+            )
+        )
         command.inputs.append(
-            Task.Command.CommandInput("otherInput", optional=True, position=2, prefix="optional-param=",
-                                             separate_value_from_prefix=False, default=None))
+            Task.Command.CommandInput(
+                "otherInput",
+                optional=True,
+                position=2,
+                prefix="optional-param=",
+                separate_value_from_prefix=False,
+                default=None,
+            )
+        )
         command = Task.Command("echo")
         command.inputs.append(
-            Task.Command.CommandInput("taskGreeting", optional=False, position=None, prefix="-a",
-                                      separate_value_from_prefix=True, default=None))
+            Task.Command.CommandInput(
+                "taskGreeting",
+                optional=False,
+                position=None,
+                prefix="-a",
+                separate_value_from_prefix=True,
+                default=None,
+            )
+        )
         command.inputs.append(
-            Task.Command.CommandInput("otherInput", optional=True, position=2, prefix="optional-param=",
-                                      separate_value_from_prefix=False, default=None))
+            Task.Command.CommandInput(
+                "otherInput",
+                optional=True,
+                position=2,
+                prefix="optional-param=",
+                separate_value_from_prefix=False,
+                default=None,
+            )
+        )
 
         # t is the task
         t.command = command
         print(t.get_string())
 
-
     @staticmethod
     def test_hello_tasks():
         # https://github.com/openwdl/wdl/#specifying-inputs-and-using-declarations
 
-        t1 = Task("hello",
-                  inputs=[Input(String, "name"), Input(String, "salutation")],
-                  outputs=[Output(String, "response", "read_string(stdout())")]
-                  )
-        t1.command = Task.Command("echo",
-                                  inputs=[Task.Command.CommandInput.from_input(t1.inputs[i], position=i) for i in
-                                          range(2)])
+        t1 = Task(
+            "hello",
+            inputs=[Input(String, "name"), Input(String, "salutation")],
+            outputs=[Output(String, "response", "read_string(stdout())")],
+        )
+        t1.command = Task.Command(
+            "echo",
+            inputs=[
+                Task.Command.CommandInput.from_input(t1.inputs[i], position=i)
+                for i in range(2)
+            ],
+        )
 
         print(t1.get_string())
 
@@ -131,21 +172,40 @@ echo \\
         self.assertEqual(expected, command.get_string())
 
     def test_commandinput_space(self):
-        t = Task.Command.CommandInput("taskGreeting", optional=False, position=None, prefix="-a",
-                                      separate_value_from_prefix=True, default=None)
+        t = Task.Command.CommandInput(
+            "taskGreeting",
+            optional=False,
+            position=None,
+            prefix="-a",
+            separate_value_from_prefix=True,
+            default=None,
+        )
         self.assertEqual("-a ~{taskGreeting}", t.get_string())
 
     def test_commandinput_nospace(self):
-        t = Task.Command.CommandInput("taskGreeting", optional=False, position=None, prefix="val=",
-                                      separate_value_from_prefix=False, default=None)
+        t = Task.Command.CommandInput(
+            "taskGreeting",
+            optional=False,
+            position=None,
+            prefix="val=",
+            separate_value_from_prefix=False,
+            default=None,
+        )
         self.assertEqual("val=~{taskGreeting}", t.get_string())
 
     def test_commandarg_space(self):
-        t = Task.Command.CommandInput("argVal", position=None, prefix="-p", separate_value_from_prefix=True)
+        t = Task.Command.CommandInput(
+            "argVal", position=None, prefix="-p", separate_value_from_prefix=True
+        )
         self.assertEqual("-p ~{argVal}", t.get_string())
 
     def test_commandarg_nospace(self):
-        t = Task.Command.CommandArgument(prefix="arg=", value="argVal", position=None, separate_value_from_prefix=False)
+        t = Task.Command.CommandArgument(
+            prefix="arg=",
+            value="argVal",
+            position=None,
+            separate_value_from_prefix=False,
+        )
         self.assertEqual("arg=argVal", t.get_string())
 
 
@@ -154,28 +214,29 @@ class TestWorkflowGeneration(unittest.TestCase):
         # https://github.com/openwdl/wdl/#specifying-inputs-and-using-declarations
         w = Workflow("test")
 
-        w.calls.append(WorkflowCall(
-            TestTaskGeneration.test_hello_tasks().name
-        ))
+        w.calls.append(WorkflowCall(TestTaskGeneration.test_hello_tasks().name))
 
-        w.calls.append(WorkflowCall(
-            TestTaskGeneration.test_hello_tasks().name,
-            alias="hello2",
-            inputs_map={
-                "salutation": '"Greetings"',
-                "name": '"Michael"'
-            }
-        ))
+        w.calls.append(
+            WorkflowCall(
+                TestTaskGeneration.test_hello_tasks().name,
+                alias="hello2",
+                inputs_map={"salutation": '"Greetings"', "name": '"Michael"'},
+            )
+        )
 
         print(w.get_string())
 
 
 class TestWorkflowScatter(unittest.TestCase):
     def test_call_scatter(self):
-        sc = WorkflowScatter("i", "integers", [
-            WorkflowCall(Task("task1").name, inputs_map={"num": "i"}),
-            WorkflowCall(Task("task2").name, inputs_map={"num": "task1.output"})
-        ])
+        sc = WorkflowScatter(
+            "i",
+            "integers",
+            [
+                WorkflowCall(Task("task1").name, inputs_map={"num": "i"}),
+                WorkflowCall(Task("task2").name, inputs_map={"num": "task1.output"}),
+            ],
+        )
 
         print(sc.get_string(indent=0))
         print(sc.get_string(indent=1))
