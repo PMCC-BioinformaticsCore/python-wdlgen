d4bbad5d6b11650f555bd95d62a47d973605a973
Michael Franklin <michael.franklin@petermac.org>
2020-03-18 11:09:12 +1100

    ParamMetaAttribute unwrap is JSON-like, write custom serializer

--- wdlgen/util.py
+++ wdlgen/util.py
@@ -9,7 +9,7 @@ def convert_python_value_to_wdl_literal(val) -> str:
     if hasattr(val, "get_string"):
         return val.get_string()
     if isinstance(val, dict):
-        return json.dumps(val)
+        return ParameterMeta.ParamMetaAttribute(**val).get_string()
 
     if isinstance(val, bool):
         return "true" if val else "false"
@@ -70,16 +70,31 @@ class Meta(WrappedKvClass):
 
 
 class ParameterMeta(WrappedKvClass):
-    class ParamMetaAttribute(WdlBase):
+    class ParamMetaAttribute(KvClass):
         def __init__(self, help=None, suggestions=None, **kwargs):
-            self.kwargs = kwargs
+            super().__init__(**kwargs)
             if help is not None:
                 self.kwargs["help"] = help
             if suggestions is not None:
                 self.kwargs["suggestions"] = suggestions
 
-        def get_string(self):
-            return json.dumps(self.kwargs or {})
+        def get_string(self, indent=0):
+            l = []
+            sorted_keys = sorted(self.kwargs.keys())
+            for k in sorted_keys:
+                v = self.kwargs[k]
+                if v is None:
+                    continue
+                val = v
+                if hasattr(v, "get_string"):
+                    val = v.get_string()
+                elif isinstance(v, dict):
+                    val = ParameterMeta(**v).get_string(indent=indent)
+                else:
+                    val = convert_python_value_to_wdl_literal(val)
+
+                l.append("{k}: {v}".format(k=k, v=val))
+            return "{" + ", ".join(l) + "}"
 
         def __setitem__(self, key, value):
             self.kwargs[key] = value
